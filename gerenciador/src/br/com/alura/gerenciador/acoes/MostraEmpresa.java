0f1cb9a70d012101961bf935aa2a334f7b13ce13
Thiago Marçal <thiagomarcal1984@gmail.com>
2022-06-06 23:01:15 -0300

    Separando ações.

--- gerenciador/src/br/com/alura/gerenciador/acoes/MostraEmpresa.java
+++ gerenciador/src/br/com/alura/gerenciador/acoes/MostraEmpresa.java
@@ -1,24 +1,18 @@
-package br.com.alura.gerenciador.servlet;
+package br.com.alura.gerenciador.acoes;
 
 import java.io.IOException;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import br.com.alura.gerenciador.modelo.Banco;
 import br.com.alura.gerenciador.modelo.Empresa;
 
-
-@WebServlet("/mostraEmpresa")
-public class MostraEmpresaServlet extends HttpServlet {
-	private static final long serialVersionUID = 1L;
-
-	
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+public class MostraEmpresa {
+	public void executa(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		System.out.println("Mostrando dados da empresa.");
 
 		String paramId = request.getParameter("id");
 		Integer id = Integer.valueOf(paramId);
@@ -34,5 +28,4 @@ public class MostraEmpresaServlet extends HttpServlet {
 		RequestDispatcher rd = request.getRequestDispatcher("/formAlteraEmpresa.jsp");
 		rd.forward(request, response);
 	}
-
 }
--- gerenciador/src/br/com/alura/gerenciador/servlet/UnicaEntradaServlet.java
+++ gerenciador/src/br/com/alura/gerenciador/servlet/UnicaEntradaServlet.java
@@ -1,13 +1,18 @@
 package br.com.alura.gerenciador.servlet;
 
 import java.io.IOException;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import br.com.alura.gerenciador.acoes.AlteraEmpresa;
 import br.com.alura.gerenciador.acoes.ListaEmpresas;
+import br.com.alura.gerenciador.acoes.MostraEmpresa;
+import br.com.alura.gerenciador.acoes.NovaEmpresa;
+import br.com.alura.gerenciador.acoes.RemoveEmpresa;
 
 /**
  * Servlet implementation class UnicaEntradaServlet
@@ -21,12 +26,18 @@ public class UnicaEntradaServlet extends HttpServlet {
 		if(paramAcao.equals("ListaEmpresas")) {
 			ListaEmpresas acao = new ListaEmpresas();
 			acao.executa(request, response);
-		}
-		if(paramAcao.equals("RemoveEmpresa")) {
-			System.out.println("Removendo empresa.");
-		}
-		if(paramAcao.equals("MostraEmpresa")) {
-			System.out.println("Mostrando dados da empresa.");
+		} else if(paramAcao.equals("RemoveEmpresa")) {
+			RemoveEmpresa acao = new RemoveEmpresa();
+			acao.executa(request, response);
+		} else if(paramAcao.equals("MostraEmpresa")) {
+			MostraEmpresa acao = new MostraEmpresa();
+			acao.executa(request, response);
+		} else if(paramAcao.equals("AlteraEmpresa")) {
+			AlteraEmpresa acao = new AlteraEmpresa();
+			acao.executa(request, response);
+		} else if(paramAcao.equals("NovaEmpresa")) {
+			NovaEmpresa acao = new NovaEmpresa();
+			acao.executa(request, response);
 		}
 	}
 
